dc8427fd8263b741ca63227af95f6be1354fa891
KMGJan <kinlan.jan@su.se>
2023-06-16 11:48:15 +0200

    updating test file

--- tests/testthat/test-str_split_one.R
+++ tests/testthat/test-str_split_one.R
@@ -1,4 +1,4 @@
-test_that("strsplit1() splits a string", {
+test_that("str_split_one() splits a string", {
   expect_equal(str_split_one("a,b,c", pattern = ","),
                c("a", "b", "c"))
 })
